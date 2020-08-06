9a21504705814700d9f26f79b7c81ccd338c49f4
DerDjamel <dermeddjamel@gmail.com>
2020-08-06 20:20:37 +0100

    added the navbar to the home page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,46 +1,57 @@
 <template>
   <v-app>
-    <v-app-bar
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
       app
-      color="primary"
-      dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      dark >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <!-- <v-img class="mr-3" alt="esiconf" src="./assets/logo.svg" max-height="40px" max-width="40px"></v-img> -->
+        <span class="hidden-sm-and-down font-weight-bold">
+          EsiConf
+        </span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-plus</v-icon>
       </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+
+    <!-- End of v-app-bar -->
     </v-app-bar>
 
+
+    <!-- Main Content -->
     <v-main>
       <router-view></router-view>
     </v-main>
+
+
   </v-app>
+  <!-- End of the v-app -->
 </template>
 
 <script>
@@ -50,7 +61,8 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer : false,
+    under: true,
   }),
 };
 </script>
